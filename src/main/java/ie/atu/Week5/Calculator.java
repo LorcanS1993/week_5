fa05b3aac9706f282b87322c565cec46da0bf081
Lorcan <G00289865@atu.ie>
2022-10-19 10:51:17 +0100

    Added new test files for Add, Subtract, Multiply and Divide

--- src/main/java/ie/atu/Week5/Calculator.java
+++ src/main/java/ie/atu/Week5/Calculator.java
@@ -1,24 +1,37 @@
 package ie.atu.Week5;
 
 public class Calculator {
+    private int calcValue;
+    private int startingValue = 4;
 
-    public Calculator() {
-
+    public Calculator(){
+        calcValue = 0;
+    }
+    public Calculator(int startingValue){
+        if(startingValue > 5) {
+            calcValue = startingValue;
+        }
+        else{
+            throw new IllegalArgumentException("This is not a valid number");
+        }
+    }
+    public int add() {
+        calcValue++;
+        return calcValue++;
     }
 
-    public int testAdd(int a, int b) {
-        return a + b;
+    public int sub() {
+       calcValue--;
+        return calcValue;
     }
 
-    public int testSubtract(int a, int b) {
-        return a - b;
+    public int multi(int first, int second) {
+        calcValue = first * second;
+        return calcValue;
     }
 
-    public long testMultiply(int a, int b) {
-        return a * b;
-    }
-
-    public double testDivide(int a, int b) {
-        return a / b;
+    public int divide(int first, int second) {
+        calcValue = first / second;
+        return calcValue;
     }
 }
