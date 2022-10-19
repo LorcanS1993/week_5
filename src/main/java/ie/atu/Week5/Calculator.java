effd83ede7d0039fbdd1f742bcd72018f5729eae
Lorcan <G00289865@atu.ie>
2022-10-19 10:21:29 +0100

    Added new file Calculator.java

--- src/main/java/ie/atu/Week5/Calculator.java
+++ src/main/java/ie/atu/Week5/Calculator.java
@@ -1,2 +1,24 @@
-package ie.atu.Week5;public class Calculator {
+package ie.atu.Week5;
+
+public class Calculator {
+
+    public Calculator() {
+
+    }
+
+    public int testAdd(int a, int b) {
+        return a + b;
+    }
+
+    public int testSubtract(int a, int b) {
+        return a - b;
+    }
+
+    public long testMultiply(int a, int b) {
+        return a * b;
+    }
+
+    public double testDivide(int a, int b) {
+        return a / b;
+    }
 }
