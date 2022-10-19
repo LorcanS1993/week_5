321bdd20f8727ac47d10f7c674bdb9498cee130c
Lorcan <G00289865@atu.ie>
2022-10-19 10:51:45 +0100

    Created new test methods in CalculatorTest

--- src/test/java/ie/atu/Week5/CalculatorTest.java
+++ src/test/java/ie/atu/Week5/CalculatorTest.java
@@ -2,20 +2,42 @@ package ie.atu.Week5;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
-
+    Calculator myCount;
     @BeforeEach
     void setUp() {
-
+        myCount = new Calculator();
+    }
+    @Test
+    void testAdd(){
+        assertEquals(1, myCount.add());
     }
 
+    @Test
+    void testSub(){
+        assertEquals(-1, myCount.sub());
+    }
 
+    @Test
+    void testMultiply(){
+        assertEquals(16, myCount.multi(4,4));
+    }
+
+    @Test
+    void testDivide(){
+        assertEquals(2, myCount.divide(4,2));
+    }
+
+    @Test
+    void testConstructor(){
+        Exception exMessage = assertThrows(IllegalAccessException.class, () -> {new Calculator(4);});
+        assertEquals("This is not a valid numner", exMessage);
+    }
     @AfterEach
     void tearDown() {
-
     }
 }
